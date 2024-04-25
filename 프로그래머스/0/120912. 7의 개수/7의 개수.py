2e606b42b7fd748a3ab0e88752282d18ae12b710
fall <134408891+fallkim@users.noreply.github.com>
2024-04-25 13:41:29 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -1,8 +1,6 @@
 def solution(array):
     answer = 0
     for i in array:
-        while i >0:
-            if i % 10 == 7:
-                answer +=1
-            i //=10
+        num= str(i)
+        answer += num.count('7')
     return answer
